4ad00061e7e5c1eee7c8ab4a5a3ffa1a7139747d
BaturalpInce <inceb20@itu.edu.tr>
2024-04-03 00:46:25 +0300

    Changed the Dropdown Indicator Select menu to a Popup menu with better UI, this will help to show any indicator which can have different time options. Added 3 different moving average options on the main graph.

--- src/hooks/useChartData.js
+++ src/hooks/useChartData.js
@@ -2,35 +2,35 @@ import React, { useState, useEffect } from 'react';
 
 // custom hook for data fetching and formatting
 export const useChartData = (url) => {
-  const fetchDataAndFormat = async () => {
-    const response = await fetch(url);
+    const fetchDataAndFormat = async () => {
+        const response = await fetch(url);
 
-    // comment below for Backend API Request
-    /* const requestBody = {
-        names: ["aapl"],
-        period: "6mo"
+        // comment below for Backend API Request
+        /* const requestBody = {
+            names: ["aapl"],
+            period: "6mo"
+        };
+
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(requestBody)
+        }); */
+
+        const data = await response.json();
+        const ohlc = data.map(item => ([item[0], item[1], item[2], item[3], item[4]])); // date, open, high, low, close
+        const volume = data.map(item => ([item[0], item[5]])); // date, volume
+            
+        return { ohlc, volume };
     };
 
-    const response = await fetch(url, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(requestBody)
-    }); */
+    const [data, setData] = useState({ ohlc: [], volume: [] });
 
-    const data = await response.json();
-    const ohlc = data.map(item => ([item[0], item[1], item[2], item[3], item[4]])); // date, open, high, low, close
-    const volume = data.map(item => ([item[0], item[5]])); // date, volume
-    
-    return { ohlc, volume };
-  };
+    useEffect(() => {
+        fetchDataAndFormat().then(setData);
+    }, []);
 
-  const [data, setData] = useState({ ohlc: [], volume: [] });
-
-  useEffect(() => {
-    fetchDataAndFormat().then(setData);
-  }, []);
-
-  return data;
+    return data;
 };
