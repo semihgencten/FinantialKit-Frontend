447a377e181bdbfbaea395c98949ee66f9178565
karadagd20 <73991457+ituitis20-karadagd20@users.noreply.github.com>
2024-04-23 17:29:21 +0300

    Updated equitiesFinancialPage based on layout

--- src/pages/EquitiesFinancialsPage/index.jsx
+++ src/pages/EquitiesFinancialsPage/index.jsx
@@ -1,4 +1,4 @@
-import { Box } from '@mui/material'
+/*import { Box } from '@mui/material'
 import TabsLayout from '@/pages/EquitiesPage';
 
 const EquitiesFinancialsPage = () => {
@@ -13,3 +13,78 @@ const EquitiesFinancialsPage = () => {
 };
 
 export default EquitiesFinancialsPage;
+*/
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import PropTypes from 'prop-types'; // Import PropTypes
+import { Box } from '@mui/material';
+
+
+const FinancialsTabsLayout = ({ children }) => {
+    const navigate = useNavigate(); 
+    const [value, setValue] = useState(getTabValueFromPath(location.pathname));
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+        switch(newValue) {
+            case 'one':
+                navigate('/equities/financials/profile');
+                break;
+            case 'two':
+                navigate('/equities/financials/dividends');
+                break;
+            case 'three':
+                navigate('/equities/financials/statements');
+                break;
+            case 'four':
+                navigate('/equities/financials/risk');
+                break;
+            case 'five':
+                navigate('/equities/financials/statistics');
+                break;
+            default:
+
+                break;
+        }
+    };
+
+    return (
+        <div>
+            <Tabs
+                value={value}
+                onChange={handleChange}
+                aria-label="wrapped label tabs example"
+                sx={{ transition: '0.3s' }} 
+            >
+                <Tab value="one" label="Profile" />
+                <Tab value="two" label="Dividends" />
+                <Tab value="three" label="Statements" />
+                <Tab value="four" label="Risk" />
+                <Tab value="five" label="Statistic" />
+            </Tabs>
+            {children}
+        </div>
+    );
+};
+const getTabValueFromPath = (path) => {
+    switch(path) {
+        case '/equities/financials/profile':
+            return 'one';
+        case '/equities/financials/dividends':
+            return 'two';
+        case '/equities/financials/statements':
+            return 'three';
+        case '/equities/financials/risk':
+            return 'four';
+        case '/equities/financials/statistics':
+            return 'five';
+        default:
+            return 'one';
+    }
+};
+FinancialsTabsLayout.propTypes = {
+    children: PropTypes.node // Validate children prop as a React node
+};
+export default FinancialsTabsLayout;
