ee3cf4e2a7996376d508d7c05a734f556842a95e
aycagokdag <gokdag19@itu.edu.tr>
2024-05-22 00:06:40 +0300

    Bugfix on portfolio slices

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import financeSlice from "@/reducers/financeSlice";
 import userSlice from "@/reducers/userSlice";
+import portfolioSlice from "@/reducers/portfoliosSlice";
+
 export default configureStore({
   reducer: {
     finance: financeSlice,
     user: userSlice,
+    portfolio: portfolioSlice,
   },
 });
