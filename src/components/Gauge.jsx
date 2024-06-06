2fc1c5deae6d91c4af163e3f285d6134d7549d0f
BaturalpInce <inceb20@itu.edu.tr>
2024-06-06 18:46:45 +0300

    Chart Add/Remove Indicator Popup design is changed. Gauge chart added to the Technicals Page.
    The tables on Technicals Page were not using data of backend API so I also connected those.

---
+++ src/components/Gauge.jsx
@@ -0,0 +1,82 @@
+import React, { useEffect } from "react";
+import Highcharts from "highcharts/highstock";
+import HighchartsMore from "highcharts/highcharts-more";
+import SolidGauge from "highcharts/modules/solid-gauge"; 
+import { Box } from "@mui/material";
+
+export const Gauge = ({id, action, confidence}) => {
+    const confidenceValue = parseFloat(confidence); 
+
+    const setupGauge = () => {
+        Highcharts.chart(id, {
+            chart: {
+                type: 'gauge',
+                plotBackgroundColor: null,
+                plotBackgroundImage: null,
+                plotBorderWidth: 0,
+                plotShadow: false,
+                height: '90%',
+            },
+            title: {
+                text: action
+            },
+            pane: {
+                startAngle: -90,
+                endAngle: 89.9,
+                background: null,
+                center: ['50%', '75%'],
+                size: '100%'
+            },
+            yAxis: {
+                min: 0,
+                max: 100,
+                tickPixelInterval: 20,
+                tickPosition: 'inside',
+                labels: {
+                    distance: 20,
+                    style: {
+                        fontSize: '14px'
+                    }
+                },
+                lineWidth: 0,
+                plotBands: [{
+                    from: 0,
+                    to: 40,
+                    color: '#55BF3B' // green
+                }, {
+                    from: 40,
+                    to: 70,
+                    color: '#DDDF0D' // yellow
+                }, {
+                    from: 70,
+                    to: 100,
+                    color: '#DF5353' // red
+                }]
+            },
+            series: [{
+                name: 'Confidence',
+                data: [confidenceValue],
+                dial: {
+                    radius: '80%',
+                    backgroundColor: 'gray',
+                    baseWidth: 12,
+                    baseLength: '0%',
+                    rearLength: '0%'
+                },
+                pivot: {
+                    backgroundColor: 'gray',
+                    radius: 6
+                }
+            }]
+        });
+        
+    }
+
+    useEffect(() => {
+        HighchartsMore(Highcharts);
+        SolidGauge(Highcharts);
+        setupGauge();
+    }, []);
+
+    return <Box id={id} style={{ height: "400px", width: "500px" }}></Box>;
+}
